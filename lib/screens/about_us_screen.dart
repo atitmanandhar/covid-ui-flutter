aab00cd5196183353115883cc9c1f89b6482be38
Atit Manandhar <atit.manandhar.a9@gmail.com>
2020-09-18 19:44:36 +0545

    Contents add in About Us page

--- lib/screens/about_us_screen.dart
+++ lib/screens/about_us_screen.dart
@@ -1,3 +1,5 @@
+import 'package:covid_ui_flutter/constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AboutUsScreen extends StatefulWidget {
@@ -9,11 +11,52 @@ class _AboutUsScreenState extends State<AboutUsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(
-        'Love Love Love',
-        style: TextStyle(
-          color: Colors.red,
-        ),
+      appBar: AppBar(
+        title: Text('About Us'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Card(
+            color: Theme.of(context).primaryColor,
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 12.0,
+                vertical: 20.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Covid Info App',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    'What this app does?',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white60,
+                    ),
+                  ),
+                  Text(
+                    'It provides you the up-to-date coronavirus statistics at global and country levels.',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      // fontWeight: FontWeight.w600,
+                      color: Colors.white60,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
